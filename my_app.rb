a6a25d752498ecaf4a203d14dcf749839ee308fb
Christina Thompson <c.thompsonms@gmail.com>
2013-11-25 08:46:58 -0800

    initial commit

---
+++ my_app.rb
@@ -0,0 +1,14 @@
+# my_app.rb
+require 'sinatra'
+
+class MyApp < Sinatra::Base
+  
+  get "/" do
+    erb :index 
+  end
+
+  get "/:cute_pictures_of_animals" do
+    erb :cute_pictures_of_animals
+  end
+
+end
