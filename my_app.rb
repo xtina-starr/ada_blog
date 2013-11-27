75a8454eb14f2d28a5282717e8d8978aca096935
Christina Thompson <c.thompsonms@gmail.com>
2013-11-27 15:20:51 -0800

    added meta code

--- my_app.rb
+++ my_app.rb
@@ -1,18 +1,38 @@
 # my_app.rb
 require 'sinatra'
+require 'yaml'
 
 class MyApp < Sinatra::Base
-  
+
+  before do
+    @posts = Dir.glob("views/posts/*.erb").map do |post_name|
+      post_name.split("/").last.slice(0..-5)
+    end
+  end
+
   get "/" do
     erb :index 
   end
 
   get "/blog/:post_name" do
-    params[:post_name]
-    erb "posts/#{params[:post_name]}".to_sym
+    page = erb("/posts/#{params[:post_name]}".to_sym, layout: false)
+    page_body = page.split("\n\n", 2).last
+    erb page_body
   end
 
-  
+  def meta_data
+    if @meta_data
+      @meta_data
+    else
+    @meta_data = {}
+    @posts.each do |post|
+      html = erb("/posts/#{post}".to_sym, layout: false)
+      meta = YAML.load(html.split("\n\n", 2).first)
+      @meta_data[post] = meta
+    end
+    @meta_data
+  end
+end
 
   # get "/name/:rred" do 
   #   puts params.inspect
