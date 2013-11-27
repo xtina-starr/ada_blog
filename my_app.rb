e0dbbd7649f3d8a0bffae532a7e21ad44d5f1709
Christina Thompson <c.thompsonms@gmail.com>
2013-11-27 11:01:38 -0800

    added blog to app

--- my_app.rb
+++ my_app.rb
@@ -7,4 +7,14 @@ class MyApp < Sinatra::Base
     erb :index 
   end
 
+  get "/blog/:post_name" do
+    params[:post_name]
+    erb "posts/#{params[:post_name]}".to_sym
+  end
+
+  
+
+  # get "/name/:rred" do 
+  #   puts params.inspect
+  # end
 end
